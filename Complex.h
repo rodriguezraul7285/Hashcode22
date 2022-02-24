b73e6a9903aaeb8cf90c763d7e7dcfd89e188e48
Raul <rodriguezraul7285@gmail.com>
2022-02-24 11:47:39 -0600

    first

---
+++ Complex.h
@@ -0,0 +1,23 @@
+//
+// Created by MSI-USER on 10/21/2021.
+//
+
+#ifndef MAIN_CPP_COMPLEX_H
+#define MAIN_CPP_COMPLEX_H
+/*
+ * Crear una clase de numeros complejos Complex con los siguientes atributos:
+	double real
+	double imaginary
+La clase debe de contar con 3 tipos de consctructores vacío,entran 2 flotantes o entra otro numero complejo.
+La clase Complex debe de tener implementadas 5 sobrecargas de contructor:
+	<< para imprimir en una sola linea los valores del objeto complex
+	+ para dos objetos complex
+	+ para un double y un objeto complex
+	+ para un objeto complex y un double
+= para dos objetos complez
+complex
+
+ */
+
+
+#endif //MAIN_CPP_COMPLEX_H
---
+++ cmake-build-debug/CMakeFiles/First_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/First_Project.dir/main.cpp.obj"
+  "First_Project.exe"
+  "First_Project.exe.manifest"
+  "First_Project.pdb"
+  "libFirst_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/First_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
