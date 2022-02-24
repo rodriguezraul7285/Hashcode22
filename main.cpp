76dd7af6fd8dc6970a95d4bf3938791eb7bfaf88
Raul <rodriguezraul7285@gmail.com>
2022-02-24 12:55:42 -0600

    complex.h deleted

--- main.cpp
+++ main.cpp
@@ -2,7 +2,55 @@
 
 using namespace std;
 
+/*
+int main () {
+    ofstream writeFile ("a_an_example.in.txt");
+    if (writeFile.is_open())
+    {
+        writeFile << "This is a line.\n";
+        writeFile << "This is another line.\n";
+        writeFile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
+}*/
 
-int main(){
-    cout<<"j\n";
+void inputFileReading(){
+    string line;
+    ifstream readFile ("a_an_example.in.txt");
+    if (readFile.is_open())
+    {
+        while ( getline (readFile, line) )
+        {
+            cout << line << '\n';
+        }
+        readFile.close();
+    }
+
+    else cout << "Unable to open file";
+
+}
+struct Skill{
+    string name;
+    short level;
+};
+struct Contribuitor{
+    string name;
+    vector<Skill> skills;
+};
+struct Project{
+    string name;
+    int duration;
+    int score;
+    int bestBefore;
+    vector<Skill> Roles;
+};
+
+
+int main () {
+    int C=0,P=0;//contributors and proyects.
+
+
+
+    return 0;
 }
